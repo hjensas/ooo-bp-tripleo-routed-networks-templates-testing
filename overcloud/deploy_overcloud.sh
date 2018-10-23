4c718b535fd08889998fffed8d67d4b903d1a450
Harald Jensås <hjensas@redhat.com>
2018-10-23 18:35:15 +0000

    Add network_data template and update deploy command

--- overcloud/deploy_overcloud.sh
+++ overcloud/deploy_overcloud.sh
@@ -4,11 +4,10 @@ source /home/stack/stackrc
 cd /home/stack
 
 openstack overcloud deploy --templates /home/stack/tripleo-heat-templates \
-  -n /home/stack/tripleo-heat-templates/network_data_subnets_routed.yaml \
+  -n /home/stack/overcloud/templates/network_data_subnets_routed.yaml \
   -r /home/stack/overcloud/templates/my_roles_data.yaml \
   -e /home/stack/overcloud/environments/node_data.yaml \
   -e /home/stack/tripleo-heat-templates/environments/network-isolation.yaml \
-  -e /home/stack/tripleo-heat-templates/environments/low-memory-usage.yaml \
   -e /home/stack/tripleo-heat-templates/environments/network-environment.yaml \
   -e /home/stack/overcloud/environments/network-environment-overrides.yaml 
 
